5370060c70a389a3d1d340eec11f0315f6f970d7
Justin Lafferty <lafferjm@gmail.com>
2019-11-02 12:39:34 -0500

    drawing the fist again

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,7 @@ int main(int argc, char** argv) {
         window->begin();
         text->draw();
         chimp->draw();
-        //fist->draw();
+        fist->draw();
         window->end();
         
     }
